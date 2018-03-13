386dd12e250a1b376292f19ecaa2b29e8a08885b
Mitrofanov Aleksey <mitrofanov_a@itstep.lan>
2018-03-13 21:35:21 +0300

    l75

---
+++ L75-sql/database/sql/create_database.sql
@@ -0,0 +1,27 @@
+
+--odnostrochnyj comment 
+
+CREATE TABLE chelovek (
+	id int NOT NULL,
+	age int NOT NULL,
+	name varchar(20) NOT NULL,
+	surname varchar(20) NOT NULL,
+	wieght int NOT NULL,
+	height int NOT NULL,
+	sex boolean NOT NULL DEFAULT '0',
+	description text
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
