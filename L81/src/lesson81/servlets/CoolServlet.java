35d727f72a91c8d63f0694eed36f91a4943c6a1c
Mitrofanov Aleksey <mitrofanov_a@itstep.lan>
2018-04-05 21:42:24 +0300

    l81

---
+++ L81/src/lesson81/servlets/CoolServlet.java
@@ -0,0 +1,49 @@
+package lesson81.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CoolServlet extends HttpServlet {
+	int counter =0;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		counter++;
+		super.service(req, resp);
+		/*if (req.getMethod().equals("POST")) {
+			doPost(req, res p);
+		}
+		if (req.getMethod().equals("GET")) {
+			doGet(req, resp);
+		}*/
+		
+		
+		//System.out.println("запрос принят, сервис");
+		//resp.getOutputStream().println("<h1>Hello</h1>");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("запрос принят, гет отработал");
+		resp.getOutputStream().println("<h1>Hello</h1>");
+		System.out.println("запрос номер " + counter 
+				+ " обраотан");
+	}
+	
+	
+	
+
+	
+}
+
+
+
+
+
+
+
+
