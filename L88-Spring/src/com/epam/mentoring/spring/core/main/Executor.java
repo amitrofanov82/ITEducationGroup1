87cff25dc7f742ccc699aadbb39aaaba5bcdd481
Mitrofanov Aleksey <mitrofanov_a@itstep.lan>
2018-05-17 21:32:01 +0300

    l88

---
+++ L88-Spring/src/com/epam/mentoring/spring/core/main/Executor.java
@@ -0,0 +1,36 @@
+package com.epam.mentoring.spring.core.main;
+
+import java.time.LocalDateTime;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.epam.mentoring.spring.core.configuration.BaseBeansConfiguration;
+import com.epam.mentoring.spring.core.configuration.ProdBeansConfiguration;
+import com.epam.mentoring.spring.core.configuration.TestBeansConfiguration;
+import com.epam.mentoring.spring.core.service.ReservationService;
+import com.epam.mentoring.spring.core.service.SessionService;
+
+public class Executor {
+
+	private static final Logger logger = Logger.getLogger(Executor.class);
+
+	// To specify active profile use VM argument:
+	// -Dspring.profiles.active="profile_name"
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context 
+			= new AnnotationConfigApplicationContext(
+				BaseBeansConfiguration.class,
+				ProdBeansConfiguration.class, 
+				TestBeansConfiguration.class);
+
+		SessionService sessionService = context.getBean(SessionService.class);
+		ReservationService reservationService = context.getBean(ReservationService.class);
+		System.out.println("Find session result: " + sessionService.findAll(LocalDateTime.of(2017, 4, 7, 15, 0)));
+		System.out.println("Reservation after find method call: " + reservationService.find("12345"));
+		reservationService.delete("12345");
+		System.out.println("Reservation after delete and find method call: " + reservationService.find("12345"));
+		context.close();
+	}
+}
---
+++ springcore/src/main/java/com/epam/mentoring/spring/core/configuration/BaseBeansConfiguration.java
@@ -0,0 +1,39 @@
+package com.epam.mentoring.spring.core.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.epam.mentoring.spring.core.dao.ReservationDao;
+import com.epam.mentoring.spring.core.dao.SessionDao;
+import com.epam.mentoring.spring.core.dao.impl.ReservationDaoImpl;
+import com.epam.mentoring.spring.core.dao.impl.SessionDaoImpl;
+import com.epam.mentoring.spring.core.service.ReservationService;
+import com.epam.mentoring.spring.core.service.SessionService;
+import com.epam.mentoring.spring.core.service.impl.ReservationServiceImpl;
+import com.epam.mentoring.spring.core.service.impl.SessionServiceImpl;
+
+@Configuration
+public class BaseBeansConfiguration {
+
+	@Bean
+	public SessionService sessionService() {
+		return new SessionServiceImpl();
+	}
+
+	@Bean
+	public ReservationService ReservationService() {
+		ReservationService reservationService = new ReservationServiceImpl();
+		reservationService.setReservationDao(reservationDao());
+		return reservationService;
+	}
+
+	@Bean(initMethod = "init")
+	public SessionDao sessionDao() {
+		return new SessionDaoImpl();
+	}
+
+	@Bean(destroyMethod = "destroy")
+	public ReservationDao reservationDao() {
+		return new ReservationDaoImpl();
+	}
+}
